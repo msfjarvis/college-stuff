af792a2c2600f89634b725bcd396354fc8ef1103
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-11 12:37:26 +0530

    rust: Add some status prints
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,12 +5,14 @@ use stack::Stack;
 use queue::Queue;
 
 fn main() {
+    println!("Stack implementation");
     let mut stack = Stack::default();
     stack.push(1);
     println!("{}", stack.peek());
     stack.pop();
     stack.pop();
 
+    println!("Queue implementation");
     let mut queue = Queue::default();
     queue.enqueue(1);
     println!("{}", queue.peek());
