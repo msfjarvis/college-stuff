967615c6ff113576b22fc05b2d9be73735a90959
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-11 12:37:19 +0530

    rust: Resolve missing returns in stack implementation
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- rust/src/stack.rs
+++ rust/src/stack.rs
@@ -7,20 +7,20 @@ pub struct Stack {
 impl Stack {
     pub fn push(&mut self, item: i32) {
         if self.top == 9 {
-            println!("Overflow!")
+            println!("Overflow!");
+            return;
         }
         self.items[self.top] = item;
         self.top += 1;
     }
 
-    pub fn pop(&mut self) -> i32 {
+    pub fn pop(&mut self) {
         if self.top == 0 {
-            println!("Underflow!")
+            println!("Underflow!");
+            return;
         }
-        let value = self.items[self.top];
         self.items[self.top] = 0;
         self.top -= 1;
-        value
     }
 
     pub fn peek(&self) -> i32 {
