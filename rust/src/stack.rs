5c886e09ddfb4ba84a79bb64a686cfac54d123ac
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-11 14:44:47 +0530

    rust: Rewrite stack implementation
    The previous iteration was not even a stack...
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- rust/src/stack.rs
+++ rust/src/stack.rs
@@ -1,29 +1,52 @@
-#[derive(Default)]
+use std::convert::TryInto;
+
 pub struct Stack {
-    top: usize,
+    top: i32,
+    pub size: i32,
     items: [i32; 10],
 }
 
-impl Stack {
-    pub fn push(&mut self, item: i32) {
-        if self.top == 9 {
-            println!("Overflow!");
-            return;
+impl Default for Stack {
+    fn default() -> Stack {
+        Stack {
+            top: -1,
+            size: 10,
+            items: [0; 10],
         }
-        self.items[self.top] = item;
+    }
+}
+
+impl Stack {
+    pub fn push(&mut self, item: i32) -> bool {
+        if self.top >= (self.size - 1) {
+            println!("Stack overflow!");
+            return false;
+        }
         self.top += 1;
+        self.items[Self::i32_to_usize(self.top)] = item;
+        println!("{} pushed into stack", item);
+        true
     }
 
-    pub fn pop(&mut self) {
-        if self.top == 0 {
-            println!("Underflow!");
-            return;
+    pub fn pop(&mut self) -> i32 {
+        if self.top < 0 {
+            println!("Stack underflow!");
+            return 0;
         }
-        self.items[self.top] = 0;
+        let item = self.items[Self::i32_to_usize(self.top)];
         self.top -= 1;
+        item
     }
 
     pub fn peek(&self) -> i32 {
-        self.items[self.top]
+        if self.top < 0 {
+            println!("Stack underflow!");
+            return 0;
+        }
+        self.items[Self::i32_to_usize(self.top)]
+    }
+
+    fn i32_to_usize(n: i32) -> usize {
+        n.try_into().unwrap()
     }
 }
