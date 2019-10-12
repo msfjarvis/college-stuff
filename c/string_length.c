6723d9d146e6fbf214de2c5f4c04ae9507175957
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-12 16:06:35 +0530

    c: Reformat
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- c/string_length.c
+++ c/string_length.c
@@ -1,26 +1,25 @@
-//C program to find length of a string using recursion
+// C program to find length of a string using recursion
 #include <stdio.h>
- 
-int string_length(char*);
- 
-int main() 
-{
-   char s[100];
- 
-   gets(s);
- 
-   printf("Length = %d\n", string_length(s));
- 
-   return 0;
+
+int string_length(char *);
+
+int main() {
+    char s[100];
+
+    gets(s);
+
+    printf("Length = %d\n", string_length(s));
+
+    return 0;
 }
- 
+
 int string_length(char *s) {
-   static int c = 0;
- 
-   while (s[c] != '\0') {
-      c++;
-      string_length(s+1);
-   }
- 
-   return c;
+    static int c = 0;
+
+    while (s[c] != '\0') {
+        c++;
+        string_length(s + 1);
+    }
+
+    return c;
 }
