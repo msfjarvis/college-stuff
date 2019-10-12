615874876dcca4f4963d86d89a4edbf4ca73dcbd
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-12 16:06:35 +0530

    c: Reformat
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- c/fibonacci.c
+++ c/fibonacci.c
@@ -1,29 +1,26 @@
-//Fibonacci series program in C language 
-#include<stdio.h>
- 
+// Fibonacci series program in C language
+#include <stdio.h>
+
 int f(int);
- 
-int main()
-{
-  int n, i = 0, c;
- 
-  scanf("%d", &n);
- 
-  printf("Fibonacci series terms are:\n");
- 
-  for (c = 1; c <= n; c++)
-  {
-    printf("%d\n", f(i));
-    i++;
-  }
- 
-  return 0;
+
+int main() {
+    int n, i = 0, c;
+
+    scanf("%d", &n);
+
+    printf("Fibonacci series terms are:\n");
+
+    for (c = 1; c <= n; c++) {
+        printf("%d\n", f(i));
+        i++;
+    }
+
+    return 0;
 }
- 
-int f(int n)
-{
-  if (n == 0 || n == 1)
-    return n;
-  else
-    return (f(n-1) + f(n-2));
+
+int f(int n) {
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return (f(n - 1) + f(n - 2));
 }
--- c/odd_even.c
+++ c/odd_even.c
@@ -1,18 +1,16 @@
-//Odd or even program in C 
+// Odd or even program in C
 #include <stdio.h>
- 
-int main()
-{
-   int n;
- 
-   printf("Enter an integer\n");
-   scanf("%d", &n);
- 
-   if (n%2 == 0)
-      printf("Even\n");
-   else
-      printf("Odd\n");
- 
-   return 0;
-}
 
+int main() {
+    int n;
+
+    printf("Enter an integer\n");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+        printf("Even\n");
+    else
+        printf("Odd\n");
+
+    return 0;
+}
