3515dd097f5b78722e0cfecdfb7720226aa2856d
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-12 15:55:22 +0530

    c: Reformat
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- c/queue_using_linked_list.c
+++ c/queue_using_linked_list.c
@@ -1,121 +1,108 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node
-{
-	int data;
-	struct node *next;
-}node;
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
 
-typedef struct
-{
-	node *start;
-}LL;
+typedef struct {
+    node *start;
+} LL;
 
-//insertEnd() of Singly LL
-void Enqueue(LL *l,int ele)
-{
-	node *newnode,*p;
-	newnode = (node*)malloc(sizeof(node));
-	newnode->data = ele;
-	newnode->next = NULL;
-	
-	if(l->start == NULL)
-	l->start = newnode;
-	else
-	{
-		p = l->start;
-		while(p->next != NULL)
-			p = p->next;
-		p->next=newnode;
-	}
+// insertEnd() of Singly LL
+void Enqueue(LL *l, int ele) {
+    node *newnode, *p;
+    newnode = (node *)malloc(sizeof(node));
+    newnode->data = ele;
+    newnode->next = NULL;
+
+    if (l->start == NULL)
+        l->start = newnode;
+    else {
+        p = l->start;
+        while (p->next != NULL)
+            p = p->next;
+        p->next = newnode;
+    }
 }
 
-//display() of Singly LL
-void display(LL *l)
-{
-	node *p;
-	if(l->start==NULL)
-	{
-		printf("Queue is empty\n");
-		return;
-	}
-	p=l->start;
-	
-	while(p!=NULL)
-	{
-		printf("%d ",p->data);
-		p=p->next;
-	}
-	printf("\n");
+// display() of Singly LL
+void display(LL *l) {
+    node *p;
+    if (l->start == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+    p = l->start;
+
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
 }
 
-//deleteBeginning() of Singly LL
-int Dequeue(LL *l)
-{
-	//p must refer to node to delete
-	node *p;
-	if(l->start == NULL)
-	{
-		return -1;
-	}
-	p = l->start;
+// deleteBeginning() of Singly LL
+int Dequeue(LL *l) {
+    // p must refer to node to delete
+    node *p;
+    if (l->start == NULL) {
+        return -1;
+    }
+    p = l->start;
     l->start = l->start->next;
-	return p->data;
+    return p->data;
 }
 
-int Queuefront(LL *l)
-{
-	if(l->start == NULL)
-		return -1;
-	else
-		return l->start->data;
+int Queuefront(LL *l) {
+    if (l->start == NULL)
+        return -1;
+    else
+        return l->start->data;
 }
 
-int main()
-{
-	int ch,ele;
-	LL l;
-	l.start=NULL;
-	while(1)
-	{
-		printf("1: Enqueue\n2: Dequeue\n3: Queue Front\n4: Display\n5: Exit\n");
-		printf("Enter choice: ");
-		scanf("%d",&ch);
-		if(ch == 5)
-			break;
-		switch(ch)
-		{
-			case 1:
-				printf("Enter element to enque: ");
-			    scanf("%d",&ele);
-			    Enqueue(&l,ele);
-			    printf("Updated queue\n");
-			    display(&l);
-			    break;
-			case 2:
-				ele=Dequeue(&l);
-			    if(ele == -1)
-					printf("Queue underflow\n");
-				else
-					printf("Element dequeued = %d\n",ele);
-				printf("Updated queue\n");
-			    display(&l);
-				break;
-			case 3:
-				ele=Queuefront(&l);
-			    if(ele == -1)
-					printf("Queue underflow\n");
-				else
-					printf("Queue front element = %d\n",ele);
-				break;
-			case 4:
-				display(&l);
-				break;
-			default:
-				printf("Invalid choice\n");
-				break;
-		}
-	}
-return 0;
+int main() {
+    int ch, ele;
+    LL l;
+    l.start = NULL;
+    while (1) {
+        printf("1: Enqueue\n2: Dequeue\n3: Queue Front\n4: Display\n5: Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+        if (ch == 5)
+            break;
+        switch (ch) {
+        case 1:
+            printf("Enter element to enque: ");
+            scanf("%d", &ele);
+            Enqueue(&l, ele);
+            printf("Updated queue\n");
+            display(&l);
+            break;
+        case 2:
+            ele = Dequeue(&l);
+            if (ele == -1)
+                printf("Queue underflow\n");
+            else
+                printf("Element dequeued = %d\n", ele);
+            printf("Updated queue\n");
+            display(&l);
+            break;
+        case 3:
+            ele = Queuefront(&l);
+            if (ele == -1)
+                printf("Queue underflow\n");
+            else
+                printf("Queue front element = %d\n", ele);
+            break;
+        case 4:
+            display(&l);
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+    }
+    return 0;
 }
--- c/stack_using_array.c
+++ c/stack_using_array.c
@@ -1,108 +1,99 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 10
 
-/*typedef keyword creates a type definition that simplifies code and makes a program easier to read.
-here stack is implemented using structure as it avoid declaring multiple arrays and their tops when multiple structures are needed.
+/*typedef keyword creates a type definition that simplifies code and makes a
+program easier to read. here stack is implemented using structure as it avoid
+declaring multiple arrays and their tops when multiple structures are needed.
 here name of the structure is "stack".*/
 
-typedef struct
-{
-	int a[MAX];
-	int top; 
-}stack;
+typedef struct {
+    int a[MAX];
+    int top;
+} stack;
 
-void push(stack *s,int ele)
-{
-	if(s->top == MAX-1)
-	{
-		printf("Stack Overflow\n");
-		return;
-	}
-	s->top++;
-	s->a[s->top] = ele;		
+void push(stack *s, int ele) {
+    if (s->top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    s->top++;
+    s->a[s->top] = ele;
 }
 
-int isempty(stack *s)
-{
-	if(s->top == -1)
-		return -1;
-	return 0;
+int isempty(stack *s) {
+    if (s->top == -1)
+        return -1;
+    return 0;
 }
 
-int pop(stack *s)
-{
-	int x;
-	if(isempty(s))
-		return -1;
-	x = s->a[s->top];
-	s->top--;
-	return x;
+int pop(stack *s) {
+    int x;
+    if (isempty(s))
+        return -1;
+    x = s->a[s->top];
+    s->top--;
+    return x;
 }
 
-int stacktop(stack *s)
-{
-	if(isempty(s))
-		return -1;
-	return s->a[s->top];
+int stacktop(stack *s) {
+    if (isempty(s))
+        return -1;
+    return s->a[s->top];
 }
 
-void display(stack *s)
-{
-	int i;
-	if(isempty(s))
-	{
-		printf("Stack Underflow\n");
-		return;
-	}
-	for(i = s->top; i >= 0; i--)
-		printf("%d\n", s->a[i]);
+void display(stack *s) {
+    int i;
+    if (isempty(s)) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    for (i = s->top; i >= 0; i--)
+        printf("%d\n", s->a[i]);
 }
 
-int main()
-{
-	stack s1; //No need to use struct keyword because typedef is used.
-	int ch,ele;
-	s1.top = -1;
-	
-	while(1)
-	{
-		printf("Menu\n1.Push\n2.Pop\n3.Stacktop\n4.Display\n5.Exit\n");
-		printf("Enter Choice : ");
-		scanf("%d",&ch);
-		if(ch == 5)
-			break;
-		
-		switch(ch)
-		{
-			case 1:
-				printf("Enter Element : ");
-				scanf("%d", &ele);
-				push(&s1, ele); //passing the stack address as an argument to the push function
-				break;
-			
-			case 2:
-				ele = pop(&s1);
-				if(ele == -1)
-					printf("Stack Underflow\n");
-				else
-					printf("Popped element = %d\n",ele);
-				break;			
-			
-			case 3:
-				ele = stacktop(&s1);
-				if(ele == -1)
-					printf("Stack Underflow\n");
-				else
-					printf("Top of stack = %d \n",ele);
-				break;
-			case 4:
-				display(&s1);
-				break;
-				
-			default:
-				printf("Invalid choice \n");
-				break;
-		}
-	}
-	return 0;
+int main() {
+    stack s1; // No need to use struct keyword because typedef is used.
+    int ch, ele;
+    s1.top = -1;
+
+    while (1) {
+        printf("Menu\n1.Push\n2.Pop\n3.Stacktop\n4.Display\n5.Exit\n");
+        printf("Enter Choice : ");
+        scanf("%d", &ch);
+        if (ch == 5)
+            break;
+
+        switch (ch) {
+        case 1:
+            printf("Enter Element : ");
+            scanf("%d", &ele);
+            push(&s1, ele); // passing the stack address as an argument to the
+                            // push function
+            break;
+
+        case 2:
+            ele = pop(&s1);
+            if (ele == -1)
+                printf("Stack Underflow\n");
+            else
+                printf("Popped element = %d\n", ele);
+            break;
+
+        case 3:
+            ele = stacktop(&s1);
+            if (ele == -1)
+                printf("Stack Underflow\n");
+            else
+                printf("Top of stack = %d \n", ele);
+            break;
+        case 4:
+            display(&s1);
+            break;
+
+        default:
+            printf("Invalid choice \n");
+            break;
+        }
+    }
+    return 0;
 }
