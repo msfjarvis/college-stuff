e5714878e589ef0a5d9370172859332a1066a8dc
simrat39 <simrats169169@gmail.com>
2019-10-14 00:01:34 +0530

    armstrong: Format a bit
    Signed-off-by: simrat39 <simrats169169@gmail.com>

--- python/armstrong.py
+++ python/armstrong.py
@@ -1,11 +1,10 @@
-
 num = int(input("Enter a number to check if its armstrong or not: "))
 
 def givePower(x,y):
     return x**y
 
-list_num = [int(x) for x in str(num)]
 
+list_num = [int(x) for x in str(num)]
 cube_list = [givePower(x,len(str(num))) for x in list_num]
 
 if sum(cube_list) == num:
