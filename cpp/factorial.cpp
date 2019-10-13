d5145ef8d6ce6edbb245b6d09e0a8ea512b2d737
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-13 17:31:47 +0530

    cpp: Reformat
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- cpp/factorial.cpp
+++ cpp/factorial.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
+#include <iostream>
 #define ll long long int
 
 using namespace std;
 
-//recursive program to calculate factorial of a given number
+// recursive program to calculate factorial of a given number
 
-ll fact(ll n){
-    return(n==1 || n==0) ? 1 : n*fact(n-1);
-}
+ll fact(ll n) { return (n == 1 || n == 0) ? 1 : n * fact(n - 1); }
 
-int main(){
+int main() {
     ll n;
-    cin>>n;
-    cout<<fact(n);
+    cin >> n;
+    cout << fact(n);
     return 0;
 }
