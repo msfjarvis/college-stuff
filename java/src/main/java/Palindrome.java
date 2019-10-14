eec3f3299e959ccb00b9692d7303f97e644c8549
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-14 20:10:46 +0530

    java: Apply spotless recommendations
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- java/src/main/java/Palindrome.java
+++ java/src/main/java/Palindrome.java
@@ -1,25 +1,20 @@
-//Palindrome program in Java; Palindrome number is a number that remains the same when its digits are reversed. Like 16461, for example, it is "symmetrical".
+// Palindrome program in Java; Palindrome number is a number that remains the same when its digits
+// are reversed. Like 16461, for example, it is "symmetrical".
 import java.util.*;
- 
-class Palindrome
-{
-   public static void main(String args[])
-   {
-      String original, reverse = ""; // Objects of String class
-      Scanner in = new Scanner(System.in);
- 
-      System.out.println("Enter a string to check if it is a palindrome");
-      original = in.nextLine();
- 
-      int length = original.length();
- 
-      for ( int i = length - 1; i >= 0; i-- )
-         reverse = reverse + original.charAt(i);
- 
-      if (original.equals(reverse))
-         System.out.println("Entered string is a palindrome.");
-      else
-         System.out.println("Entered string isn't a palindrome.");
- 
-   }
+
+class Palindrome {
+    public static void main(String args[]) {
+        String original, reverse = ""; // Objects of String class
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a string to check if it is a palindrome");
+        original = in.nextLine();
+
+        int length = original.length();
+
+        for (int i = length - 1; i >= 0; i--) reverse = reverse + original.charAt(i);
+
+        if (original.equals(reverse)) System.out.println("Entered string is a palindrome.");
+        else System.out.println("Entered string isn't a palindrome.");
+    }
 }
--- java/src/main/java/PrimeNumbers.java
+++ java/src/main/java/PrimeNumbers.java
@@ -1,39 +1,32 @@
-//Program to check if a number is a Prime number or not.
+// Program to check if a number is a Prime number or not.
 import java.util.*;
- 
-class PrimeNumbers
-{
-   public static void main(String args[])
-   {
-      int n, status = 1, num = 3;
- 
-      Scanner in = new Scanner(System.in);
-      System.out.println("Enter the number of prime numbers you want");
-      n = in.nextInt();
- 
-      if (n >= 1)
-      {
-         System.out.println("First "+n+" prime numbers are :-");
-         System.out.println(2);
-      }
- 
-      for ( int count = 2 ; count <=n ;  )
-      {
-         for ( int j = 2 ; j <= Math.sqrt(num) ; j++ )
-         {
-            if ( num%j == 0 )
-            {
-               status = 0;
-               break;
+
+class PrimeNumbers {
+    public static void main(String args[]) {
+        int n, status = 1, num = 3;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of prime numbers you want");
+        n = in.nextInt();
+
+        if (n >= 1) {
+            System.out.println("First " + n + " prime numbers are :-");
+            System.out.println(2);
+        }
+
+        for (int count = 2; count <= n; ) {
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    status = 0;
+                    break;
+                }
             }
-         }
-         if ( status != 0 )
-         {
-            System.out.println(num);
-            count++;
-         }
-         status = 1;
-         num++;
-      }         
-   }
+            if (status != 0) {
+                System.out.println(num);
+                count++;
+            }
+            status = 1;
+            num++;
+        }
+    }
 }
