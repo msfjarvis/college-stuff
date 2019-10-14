e31dbe00fd02d66f37ded883df5bd7575eff7f20
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-14 20:10:46 +0530

    java: Apply spotless recommendations
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- java/src/main/java/FahrenheitToCelsius.java
+++ java/src/main/java/FahrenheitToCelsius.java
@@ -1,16 +1,16 @@
-//Java program to convert Fahrenheit to Celsius
+// Java program to convert Fahrenheit to Celsius
 import java.util.*;
- 
+
 class FahrenheitToCelsius {
-  public static void main(String[] args) {
-    float temperatue;
-    Scanner in = new Scanner(System.in);      
- 
-    System.out.println("Enter temperatue in Fahrenheit");
-    temperatue = in.nextInt();
- 
-    temperatue = ((temperatue - 32)*5)/9;
- 
-    System.out.println("Temperatue in Celsius = " + temperatue);
-  }
+    public static void main(String[] args) {
+        float temperatue;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter temperatue in Fahrenheit");
+        temperatue = in.nextInt();
+
+        temperatue = ((temperatue - 32) * 5) / 9;
+
+        System.out.println("Temperatue in Celsius = " + temperatue);
+    }
 }
