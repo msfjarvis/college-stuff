90ec1b76f38ec0ef20a9d71572467b3d3761a5fa
Aditya Singh <baddrink.887@gmaill.com>
2019-10-28 13:34:45 +0530

    cpp: Add some core OOP programs

---
+++ cpp/function_overloading.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print(int i)
+{
+	cout << i << endl;
+}
+
+void print(double i)
+{
+	cout << i << endl;;
+}
+
+void print(char const* c)
+{
+	cout << c <<endl;;
+}
+
+int main()
+{
+	print(10);
+	print(10.25);
+	print("one");
+}
